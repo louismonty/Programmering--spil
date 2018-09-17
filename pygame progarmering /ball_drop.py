cd3b332e086d42e0261c1bdd0c516e6645fd7a0c
louismonty <louis.mail.dk@gmail.com>
2018-09-17 19:27:50 +0200

    ball v3

--- pygame progarmering /ball_drop.py
+++ pygame progarmering /ball_drop.py
@@ -1,22 +1,54 @@
 import pygame
-import math 
+import math
+from random import randint
 
 screen_width = 1000
 screen_hight = 1000
+højde = randint(1,42)
+x_ball = højde*9 + 100
 
 Rundetaarn = pygame.image.load("rundetaarn(1).png")
 screen = pygame.display.set_mode((screen_width,screen_hight))
 pygame.display.init()
+pygame.init()
+screen.fill([255,255,255])
 
-run = True
-while run:
-    pygame.time.delay(1)
+def message_display(text):
+    font = pygame.font.Font("Crimson-Roman.ttf",20)
+    text_surf = font.render(text,True,([0,0,0]))
+    screen.blit(text_surf,[10,10])
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run=False
+def gamestart():
+    text1 = ("En bold bliver smidt ud fra Rundetårn i en højde af ",højde,"Meter")
 
-    pygame.draw.circle(screen,(0, 64, 64, 64),(90,100),20,20)
-    screen.blit(Rundetaarn, (100,100))
-    pygame.display.update()
-pygame.quit()
+def gameend():
+    tid_gæt = float(input("Hvor lang tid tror du det tager i sekunder?: "))
+
+    tid = math.sqrt((-højde)/(1/2*(-9.82)))
+    print ("\nBolden rammer jorden efter ",tid, " sekunder")
+
+    afvigelse = abs(tid-tid_gæt)
+    print("\nDu gættede forkert med ",afvigelse," sekunder")
+
+    procentvis_afvigelse = int((abs(tid - tid_gæt) / tid) *100)
+    print("Din procentvise afvigelse var",procentvis_afvigelse,"%")
+
+def gameloop():
+    run = True
+    while run:
+        pygame.time.delay(1)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run=False
+            
+        message_display("En bold bliver smidt ud fra Rundetårn i en højde af ")
+        pygame.draw.circle(screen,(0, 64, 64, 64),(70,x_ball),20,20)
+        screen.blit(Rundetaarn, (100,100))
+        pygame.display.update()
+    pygame.quit()
+print(højde)
+print(x_ball)
+gameloop()
+
+
--- pygame progarmering /ball_drop_physics.py
+++ pygame progarmering /ball_drop_physics.py
@@ -2,15 +2,17 @@ import math
 from random import randint
 
 højde = randint(1,42)
-print("En bold bliver smidt ud fra Rundetårn i en højde af ",højde,"Meter")
 
-tid_gæt = float(input("Hvor lang tid tror du det tager i sekunder?: "))
+def gamestart():
+    print("En bold bliver smidt ud fra Rundetårn i en højde af ",højde,"Meter")
 
-tid = math.sqrt((-højde)/(1/2*(-9.82)))
-print ("\nBolden rammer jorden efter ",tid, " sekunder")
+    tid_gæt = float(input("Hvor lang tid tror du det tager i sekunder?: "))
 
-afvigelse = abs(tid-tid_gæt)
-print("\nDu gættede forkert med ",afvigelse," sekunder")
+    tid = math.sqrt((-højde)/(1/2*(-9.82)))
+    print ("\nBolden rammer jorden efter ",tid, " sekunder")
 
-procentvis_afvigelse = int((abs(tid - tid_gæt) / tid) *100)
-print("Din procentvise afvigelse var",procentvis_afvigelse,"%")
+    afvigelse = abs(tid-tid_gæt)
+    print("\nDu gættede forkert med ",afvigelse," sekunder")
+
+    procentvis_afvigelse = int((abs(tid - tid_gæt) / tid) *100)
+    print("Din procentvise afvigelse var",procentvis_afvigelse,"%")
