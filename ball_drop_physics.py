8ccc59c768a12a5d8c931be69e4252f2f3ebace6
AlbertGejr <43333325+AlbertGejr@users.noreply.github.com>
2018-09-17 12:45:28 +0200

    Add files via upload

---
+++ ball_drop_physics.py
@@ -0,0 +1,16 @@
+import math
+from random import randint
+
+højde = randint(1,42)
+print("En bold bliver smidt ud fra Rundetårn i en højde af ",højde,"Meter")
+
+tid_gæt = float(input("Hvor lang tid tror du det tager i sekunder?: "))
+
+tid = math.sqrt((-højde)/(1/2*(-9.82)))
+print ("\nBolden rammer jorden efter ",tid, " sekunder")
+
+afvigelse = abs(tid-tid_gæt)
+print("\nDu gættede forkert med ",afvigelse," sekunder")
+
+procentvis_afvigelse = int((abs(tid - tid_gæt) / tid) *100)
+print("Din procentvise afvigelse var",procentvis_afvigelse,"%")
